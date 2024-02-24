8742d523f7a8711a1db2972a2345f5f35ff889f5
Matthew Martinez <124634024+matttiepie@users.noreply.github.com>
2024-02-24 11:27:54 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify
+import json
+import pymysql
+
+app = Flask(__name__)
+
+music_list = [
+    {
+        "id": 0,
+        "musician": "Tame Impala",
+        "language": "English",
+        "title": "The Less I Know The Better"
+    },
+    {
+        "id": 1,
+        "musician": "Kanye West",
+        "language": "English",
+        "title": "Runaway"
+    },
+    {
+        "id": 2,
+        "musician": "Creed",
+        "language": "English",
+        "title": "My Sacrifice"
+    },
+    {
+        "id": 3,
+        "musician": "The Eagles",
+        "language": "English",
+        "title": "Hotel California"
+    }
+]
+def db_connection():
+    conn=None
+    try:
+       conn = pymysql.connect(
+        host="localhost",
+        port=3306,
+        database="test",
+        user="root",
+        password="1qaz2wsx!QAZ@WSX",
+        cursorclass=pymysql.cursors.DictCursor
+        )
+    
+    except pymysql.Error as e:
+        print(e)
+    return conn
+
+@app.route('/')
+def print_hello():
+    return 'hello world'
+
+
+@app.route('/music', methods=['GET', 'POST'])
+def music():
+    conn=db_connection()
+    cursor=conn.cursor()
+    
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM music")
+        musics= [
+            dict(id=row['id'], musician=row['musician'],
+            language=row['language'],title=row['title'])
+            for row in cursor.fetchall()
+        ]
+        if musics is not None:
+            return jsonify(musics)
+
+    if request.method == "POST":
+        new_musician = request.form['musician']
+        new_lang = request.form['language']
+        new_title = request.form['title']
+        new_music_id = cursor.lastrowid+1
+        sql="""INSERT INTO music (id, musician, language, title) VALUES (%s,%s,%s)"""
+        cursor=cursor.execute(sql,(new_music_id,new_musician,new_lang,new_title))
+        conn.commit()
+        new_music_id = cursor.lastrowid
+        return "complete"
+
+if __name__ == '__main__':
+    app.run(debug=True)
