133ef03a902252ec98a15e71d597792690e9ed8f
Matthew Martinez <124634024+matttiepie@users.noreply.github.com>
2024-02-24 18:48:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ import pymysql
 
 app = Flask(__name__)
 
-
 def db_connection():
     conn=None
     try:
        conn = pymysql.connect(
-        host="localhost",
-        port=3306,
-        database="test",
-        user="root",
+        host="partygoer.mysql.database.azure.com",
+        database="herewego",
+        user="matthewmartinez",
         password="1qaz2wsx!QAZ@WSX",
         cursorclass=pymysql.cursors.DictCursor
         )
@@ -52,28 +50,26 @@ def music():
     
 @app.route('/addUser', methods=['POST'])
 def addUser():
-    conn = db_connection()
-    cursor = conn.cursor()
-    
-    # Retrieve data from the form
+    conn=db_connection()
+    cursor=conn.cursor()
+   # Retrieve data from the form
     new_user = request.form['USERNAME']
     new_password = request.form['PASSWORD']
 
-    # Check if the username already exists
-    check_sql = "SELECT * FROM user WHERE USERNAME = %s"
+# Check if the username already exists
+    check_sql = "SELECT * FROM login WHERE USERNAME = %s"
     cursor.execute(check_sql, (new_user,))
     existing_user = cursor.fetchone()
 
     if existing_user:
         error_message = "Username already exists. Please choose a different username."
-        return render_template('index.html', error=error_message)
+        return render_template('next.html')
     else:
         # Insert the new user if the username doesn't exist
-        sql = "INSERT INTO user (USERNAME, PASSWORD) VALUES (%s, %s)"
+        sql = "INSERT INTO login (USERNAME, PASSWORD) VALUES (%s, %s)"
         cursor.execute(sql, (new_user, new_password))
         conn.commit()
         return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
